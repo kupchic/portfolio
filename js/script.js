dfad8c02f4fc3ed6c7e8f7a646d097b600a2637c
Kupchenya Aleksandr <kup4enya1999@gmail.com>
2020-11-05 12:35:44 +0300

    add a few sites

--- js/script.js
+++ js/script.js
@@ -1,28 +1,18 @@
  new WOW().init();
  
  var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    // direction: 'vertical',
-    // loop: true,
     speed: 1000,
     loop:true,
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable:true,
     },
-
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    //   hideOnClick:false,
-    //   hiddenClass:'swiper-button-hidden',
-    //   disabledClass:'swiper-button-disable'
     },
     autoplay: {
       delay: 5000,
     },
-   
 
-})
+});
